ed8d0f65e859ea2d888cf4775b57a57290c5adfa
Robert Nikolai Reith <rreith@seemoo.tu-darmstadt.de>
2020-06-30 18:35:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+## scripthunter
+
+Scripthunter is a tool that finds javascript files for a given website. To scan goole, simply run `./scripthunter.sh https://google.com`. Note that it may take a while, which is why scripthunter also implements a notification mechanism to inform you when a scan is finished via Telegram API. [Blogpost]()
+![logo](logo.png)
+
+### Setup
+
+To install scripthunter, clone this repository. Scripthunter relies on a couple of tools to be installed so make sure you have them:
+- gau
+- ffuf
+- hakrawler
+- httpx
+
+Furthermore, scripthunter uses Telegram to send you a notification once a scan is finished. To enable this feature, you need to create a Telegram Bot and paste your Bot API key and chatid in the scripthunter script.
+You can follow [this](https://blog.r0b.re/automation/bash/2020/06/30/setup-telegram-notifications-for-your-shell.html) guide to get these values.
+![telegram](telegram.png)
+
+### Features
+
+- Extract public javascript files from website using Gau and Hakrawler
+- Parse directories containing js files from found public files
+- Scan js directories for hidden js files using ffuf and a custom wordlist
+- check all found files for connectivity
+- notify user once scans are finished
+- aggregate all seen js filenames into one global wordlist
+
+### Example
+I ran this on some random verizon subdomain:
+```bash
+➜  scripthunter-dev ./scripthunter.sh https://developer.verizonmedia.com/
+               _      __  __             __
+  ___ ________(_)__  / /_/ /  __ _____  / /____ ____
+ (_-</ __/ __/ / _ \/ __/ _ \/ // / _ \/ __/ -_) __/
+/___/\__/_/ /_/ .__/\__/_//_/\_,_/_//_/\__/\__/_/
+             /_/
+                             by @r0bre
+[*] Running GAU
+[+] GAU found 7 scripts!
+[*] Running hakrawler
+[+] HAKRAWLER found 5 scripts!
+[*] Found 2 directories containing .js files.
+[*] Running FFUF on https://developer.verizonmedia.com/./
+
+[+] FFUF found 0 scripts in https://developer.verizonmedia.com/./ !
+[*] Running FFUF on https://developer.verizonmedia.com/assets/
+
+[+] FFUF found 0 scripts in https://developer.verizonmedia.com/assets/ !
+[*] Running FFUF on https://developer.verizonmedia.com/assets/js/
+
+[+] FFUF found 0 scripts in https://developer.verizonmedia.com/assets/js/ !
+[*] Running FFUF on https://developer.verizonmedia.com/js/
+
+[+] FFUF found 0 scripts in https://developer.verizonmedia.com/js/ !
+[*] Running FFUF on https://developer.verizonmedia.com/static/
+
+[+] FFUF found 0 scripts in https://developer.verizonmedia.com/static/ !
+[*] Running FFUF on https://developer.verizonmedia.com/static/js/
+
+[+] FFUF found 7 scripts in https://developer.verizonmedia.com/static/js/ !
+[*] Running FFUF on https://developer.verizonmedia.com/static/js/vendor/
+
+[+] FFUF found 3 scripts in https://developer.verizonmedia.com/static/js/vendor/ !
+[+] Checking Script Responsiveness of 13 scripts..
+https://developer.verizonmedia.com/static/js/vendor/js-cookie.js
+https://developer.verizonmedia.com/static/js/jquery-3.3.1.min.js
+https://developer.verizonmedia.com/static/js/autotrack.js
+https://developer.verizonmedia.com/static/js/utils.js
+https://developer.verizonmedia.com/static/js/navigation.js
+https://developer.verizonmedia.com/static/js/vendor/rapidworker-1.2.js
+https://developer.verizonmedia.com/static/js/vmdn.js
+https://developer.verizonmedia.com/static/js/right-nav.js
+[+] All Done!
+[+] Found total of 13 (8 responsive) scripts!
+```
+
+
+If you like this tool, consider following me on Twitter [@r0bre](https://twitter.com/r0bre)! ;)
+
---
+++ scripthunter.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+# scripthunter.sh
+# find javascript files for a website
+# Usage: # scripthunter.sh https://www.google.de
+# (c) @r0bre 2020
+# contact: mail [at] r0b.re
+#set -euo pipefail
+
+trap ctrl_c INT
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 &&pwd)"
+TMPDIR="/tmp/scripthunter"
+WLDIR="${DIR}/wordlists"
+wordlist="$WLDIR/scripthunter-wordlist.txt"
+jsdirwl="$WLDIR/jsdirs-common.txt"
+aggr="$WLDIR/aggregated.txt"
+mkdir -p $TMPDIR/results
+
+ctrl_c(){
+    echo "interrupt detected, doing cleanup and exiting.."
+    do_cleanup
+    exit 
+}
+do_cleanup(){
+    rm -rf $TMPDIR/*.txt
+    rm -rf $TMPDIR/*.json
+}
+banner(){
+echo "               _      __  __             __         "
+echo "  ___ ________(_)__  / /_/ /  __ _____  / /____ ____"
+echo " (_-</ __/ __/ / _ \/ __/ _ \/ // / _ \/ __/ -_) __/"
+echo "/___/\__/_/ /_/ .__/\__/_//_/\_,_/_//_/\__/\__/_/   "
+echo "             /_/   "
+echo "                             by @r0bre"
+}
+usage(){
+    echo "Usage: ./scripthunter.sh [-s] [Target URL]"
+    echo "       -s: Silent output - No banner, no progress, only urls"
+}
+
+tnotify(){
+    # Follow this to find your token and chatid
+    # https://medium.com/@ManHay_Hong/how-to-create-a-telegram-bot-and-send-messages-with-python-4cf314d9fa3e
+    message=$1
+    token="CHANGEME"
+    chatid="CHANGEME"
+    curl -s -X POST https://api.telegram.org/bot$token/sendMessage -d chat_id=$chatid -d text="$message" >/dev/null
+}
+if [ $# -eq 0 ] || [ "$1" = "-h" ]
+  then
+    usage
+    exit
+fi
+if [ "$1" = "-s" ] && [ $# -eq 1 ]
+then
+    echo "Please Provide a URL!"
+    usage
+    exit
+fi
+
+silent=false
+if [ "$1" = "-s" ] && [ $# -eq 2 ]
+then
+    silent=true
+    set -- "$2" # $1=$2
+fi
+
+if [ "$silent" = "false" ]; then
+    banner
+fi
+
+target=`echo "$1" | unfurl format "%s://%d%:%P"`
+domain=`echo "$1"| unfurl domain`
+if [ "$silent" = "false" ]; then
+    echo "[*] Running GAU"
+fi
+echo "$target" | gau | unfurl format "%s://%d%:%P%p" | grep -iE "\.js$" | sort -u > $TMPDIR/gaujs.txt
+gaucount="$(wc -l $TMPDIR/gaujs.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+if [ "$silent" = "false" ]; then
+    echo "[+] GAU found $gaucount scripts!"
+fi
+
+if [ "$silent" = "false" ]; then
+    echo "[*] Running hakrawler"
+fi
+hakrawler -js -url $target -plain -depth 2 -scope strict -insecure > $TMPDIR/hakrawl1.txt
+cat $TMPDIR/hakrawl1.txt| unfurl format "%s://%d%:%P%p" | grep -iE "\.js$" | sort -u > $TMPDIR/hakrawler.txt
+hakcount="$(wc -l $TMPDIR/hakrawler.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+if [ "$silent" = "false" ]; then
+    echo "[+] HAKRAWLER found $hakcount scripts!"
+fi
+
+cat $TMPDIR/gaujs.txt $TMPDIR/hakrawler.txt | sort -u > $TMPDIR/gauhak.txt
+cat $TMPDIR/gauhak.txt | unfurl format "%s://%d%:%P%p" | grep "\.js$" | rev | cut -d "/" -f2- | rev | sort -u > $TMPDIR/jsdirs.txt
+touch $TMPDIR/ffuf.txt
+jsdircount="$(wc -l $TMPDIR/jsdirs.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+if [ "$silent" = "false" ]; then
+    echo "[*] Found $jsdircount directories containing .js files."
+fi
+cat $jsdirwl | while read knowndir; do
+    echo "$target/$knowndir" >> $TMPDIR/jsdirs.txt
+done
+cat $TMPDIR/jsdirs.txt | sort -u | while read jsdir; do
+
+    if [ "$silent" = "false" ]; then
+        echo "[*] Running FFUF on $jsdir/"
+    fi
+    # for more thorough, add .min.js,.common.js,.built.js,.chunk.js,.bundled.js,...
+    ffuf -w $wordlist -u $jsdir/FUZZ -e .js,.min.js -mc 200,304 -o $TMPDIR/ffuf.json -s -t 100 > /dev/null
+    cat $TMPDIR/ffuf.json | jq -r ".results[].url" | grep "\.js" | unfurl format "%s://%d%:%P%p" | grep -iE "\.js$" | sort -u >$TMPDIR/ffuf_tmp.txt
+    cat $TMPDIR/ffuf_tmp.txt >> $TMPDIR/ffuf.txt
+    ffuftmpcount="$(wc -l $TMPDIR/ffuf_tmp.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+    if [ "$silent" = "false" ]; then
+        echo "[+] FFUF found $ffuftmpcount scripts in $jsdir/ !"
+    fi
+done
+#echo "[*] Running initial LinkFinder"
+#python3 /opt/LinkFinder/linkfinder.py -d -i $target -o cli >> linkfinder.txt
+
+
+cat $TMPDIR/gauhak.txt $TMPDIR/ffuf.txt | grep "\.js" | grep -v "Running against:" |sort -u > $TMPDIR/results/scripts-$domain.txt
+linecount="$(wc -l $TMPDIR/results/scripts-$domain.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+if [ "$silent" = "false" ]; then
+    echo "[+] Checking Script Responsiveness of $linecount scripts.."
+fi
+cat $TMPDIR/results/scripts-$domain.txt | httpx -status-code -silent -no-color | grep -E '\[200\]$' | cut -d " " -f1 | tee -a $TMPDIR/results/scripts-200-$domain.txt
+responsivecount="$(wc -l $TMPDIR/results/scripts-200-$domain.txt | sed -e 's/^[[:space:]]*//' | cut -d " " -f 1)"
+
+tnotify "Scripthunter on $target done. $linecount ($responsivecount responsive) script files found"
+if [ "$silent" = "false" ]; then
+    echo "[+] All Done!"
+    echo "[+] Found total of $linecount ($responsivecount responsive) scripts!"
+fi
+
+# Save All Seen js filenames to $aggr wordlist, which we can use in the future
+cat $TMPDIR/results/scripts-$domain.txt | rev | cut -d "/" -f1 | rev | sort -u >> $aggr
+
+do_cleanup
